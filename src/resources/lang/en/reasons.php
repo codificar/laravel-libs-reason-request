77cbaf21a9ee5a44c2e41708e9f94a7d060e64ba
João Aires <airesjoao25@outlook.com>
2021-01-06 15:06:10 -0300

    traduções

---
+++ src/resources/lang/en/reasons.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'add_reason' => 'Add request reason',
+    'add_title' => 'Add reason',
+    'list_reason' => 'List reason',
+    'filter' => 'Filter',
+    'reason' => 'Reason for request',
+    'save' => 'Save',
+    'save_success' => 'Reason success saved!',
+    'save_error' => 'Error on saving reason!',
+    'action' => 'Action',
+    'actions' => 'Actions',
+    'edit' => 'Edit',
+    'setting' => 'Active module reason',
+    'active' => 'Active module',
+    'yes' => 'Yes',
+    'no' => 'No'
+];
---
+++ src/resources/lang/pt-ao/reasons.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'add_reason' => 'Adicionar motivo para requisição',
+    'add_title' => 'Adicionar motivo',
+    'list_reason' => 'Lista de motivos',
+    'filter' => 'Filtrar',
+    'reason' => 'Motivo para requisição',
+    'save' => 'Salvar',
+    'save_success' => 'Motivo salvo com sucesso!',
+    'save_error' => 'Erro ao salvar motivo!',
+    'action' => 'Ação',
+    'actions' => 'Ações',
+    'edit' => 'Editar',
+    'setting' => 'Ativar módulo de motivos',
+    'active' => 'Ativar módulo',
+    'yes' => 'Sim',
+    'no' => 'Não'
+];
